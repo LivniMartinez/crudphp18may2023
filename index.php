906ad7e93ee370ceb6b777b53831bfb660a6593a
Livni <llmc0383@gmail.com>
2023-05-18 11:09:26 -0600

    se manda a llamar la tabla

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
 require "./modelos/Conexion.php";
-$resultado = Conexion::ejecutar("INSERT INTO productos(producto_nombre, producto_precio) values ('prueba','5')");
+$resultado = Conexion::servir("SELECT *FROM PRODUCTOS");
+echo "<pre>";
 var_dump($resultado);
+echo "<pre>";
