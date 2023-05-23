8a0f58bd7dfb2c0c12df7d03b4f939b97eb7a4ae
Livni <llmc0383@gmail.com>
2023-05-23 14:26:01 -0600

    index

--- vistas/productos/index.php
+++ vistas/productos/index.php
@@ -11,7 +11,7 @@
     <div class="container">
         <h1 class="text-center">Formulario de productos</h1>
         <div class="row justify-content-center">
-            <form action="/crudphp18may2023/controladores/productos/guardar.php" method = "POST" class="col-lg-8 border bg-light p-3">
+            <form action="/crudphp18may2023/controladores/productos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
                         <label for="producto_nombre">Nombre del producto</label>
@@ -21,7 +21,7 @@
                 <div class="row mb-3">
                     <div class="col">
                         <label for="producto_precio">Precio del producto</label>
-                        <input type="text" name="producto_precio" id="producto_precio" class="form-control">
+                        <input type="number" step="0.01" min="0" name="producto_precio" id="producto_precio" class="form-control">
                     </div>
                 </div>
                 <div class="row mb-3">
