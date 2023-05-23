6f73f0283e30083095f4150f4857d8c74fabcb93
Livni <llmc0383@gmail.com>
2023-05-23 15:47:53 -0600

    modificacion de buscar

--- vistas/productos/buscar.php
+++ vistas/productos/buscar.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>App de Tienda</title>
-</head>
-<body>
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
     <div class="container">
         <h1 class="text-center">Buscar productos</h1>
         <div class="row justify-content-center">
-            <form action="/crudphp18may2023/controladores/productos/buscar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <form action="/crudphp18may2023/controladores/productos/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
                         <label for="producto_nombre">Nombre del producto</label>
@@ -32,5 +24,4 @@
             </form>
         </div>
     </div>
-</body>
-</html>
+    <?php include_once '../../includes/footer.php'?>
