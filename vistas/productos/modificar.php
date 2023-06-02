45f785fe3db4240a7e3ecfeb8d101424bae9aff7
Livni <llmc0383@gmail.com>
2023-06-02 14:13:10 -0600

    Se actualizo el codigo

--- vistas/productos/modificar.php
+++ vistas/productos/modificar.php
@@ -1,22 +1,36 @@
 <?php
-    var_dump($_GET);
+require '../../modelos/Producto.php';
+    try {
+        $producto = new Producto($_GET);
+
+        $productos = $producto->buscar();
+        // echo "<pre>";
+        // var_dump($productos[0]['PRODUCTO_ID']);
+        // echo "</pre>";
+        // exit;
+        // $error = "NO se guardó correctamente";
+    } catch (PDOException $e) {
+        $error = $e->getMessage();
+    } catch (Exception $e2){
+        $error = $e2->getMessage();
+    }
 ?>
 <?php include_once '../../includes/header.php'?>
     <div class="container">
-        <h1 class="text-center">Modificar productos</h1>
+        <h1 class="text-center">Modificar Productos</h1>
         <div class="row justify-content-center">
-            <form action="/crudphp18may2023/controladores/productos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
-                <input type="hidden" name="producto_id">
+            <form action="/crudphp18may2023/controladores/productos/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
+                <input type="hidden" name="producto_id" value="<?= $productos[0]['PRODUCTO_ID'] ?>" >
                 <div class="row mb-3">
                     <div class="col">
                         <label for="producto_nombre">Nombre del producto</label>
-                        <input type="text" name="producto_nombre" id="producto_nombre" class="form-control">
+                        <input type="text" name="producto_nombre" id="producto_nombre" class="form-control" value="<?= $productos[0]['PRODUCTO_NOMBRE'] ?>">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
                         <label for="producto_precio">Precio del producto</label>
-                        <input type="number" step="0.01" min="0" name="producto_precio" id="producto_precio" class="form-control">
+                        <input type="number" step="0.01" min="0" name="producto_precio" id="producto_precio" class="form-control" value="<?= $productos[0]['PRODUCTO_PRECIO'] ?>">
                     </div>
                 </div>
                 <div class="row mb-3">
